3576db5cd22bb48ad0027d1a9df1ebb8911828b9
Orlando Cano <orlando.cano@kroger.com>
2022-11-07 21:09:50 -0600

    Create React Application for ITCG Library application demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import ListbookComponent from "./component/book/ListBookComponent";
+import AddbookComponent from "./component/book/AddBookComponent";
+import EditbookComponent from "./component/book/EditBookComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <Router>
+          <div className="col-md-6">
+            <h1 className="text-center" style={style}>React book Application</h1>
+            <Switch>
+              <Route path="/" exact component={ListbookComponent} />
+              <Route path="/books" component={ListbookComponent} />
+              <Route path="/add-book" component={AddbookComponent} />
+              <Route path="/edit-book" component={EditbookComponent} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
   );
 }
 
+const style = {
+  color: 'green',
+  margin: '10px'
+}
 export default App;
